bed08e24707a49c2d9469d9718383caf7a39e40a
songxu <songxu@meituan.com>
2017-06-17 18:54:46 +0800

    delete oh-my-zsh install script

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-# chsh -s `which zsh`
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # install fonts
 FONTS="$HOME/fonts"
 env git clone 'https://github.com/powerline/fonts.git' $FONTS || {
