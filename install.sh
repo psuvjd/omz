4e20de4d774d3db2a3193d8ea0e1ad12ef0b9004
songxu <songxu@meituan.com>
2017-06-17 18:00:54 +0800

    add install script、Monaco font and powerline theme

---
+++ install.sh
@@ -0,0 +1,23 @@
+# chsh -s `which zsh`
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install fonts
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+# install other fonts patch
+cp fonts/* $HOME/Library/Fonts/
+
+# copy powerline theme
+mkdir -p ~/.oh-my-zsh/custom/themes
+cp themes/powerline.zsh-theme ~/.oh-my-zsh/custom/themes
+
+# change default theme to powerline
+sed -ie 's/^ZSH_THEME.*$/ZSH_THEME="powerline"/g' ~/.zshrc
+source ~/.zshrc
+
+# hint
+echo "install already done, please change your terminal's font to your favorite one whose name contains Powerline"
+
